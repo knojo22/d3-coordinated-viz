188fdef889af4a860f5084cf7981467e32491af1
Jon Fok <jfok05@gmail.com>
2017-04-02 11:32:23 -0500

    Committ updated background layer
    conversion of background layer from census tracts to states to improve
    loading times.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
 /* JavaScript by Jon Fok, 2017 */
+(function(){
+
+var attrArray = ["HH15","HH14","HH13","HH12","HH11","HHNV_15","HHNV_14","HHNV_13","HHNV_12","HHNV_11","HH1V_15","HH1V_14","HH1V_13","HH1V_12","HH1V_11","HH2V_15","HH2V_14","HH2V_13","HH2V_12","HH2V_11","HH3V_15","HH3V_14","HH3V_13","HH3V_12","HH3V_11","HH4V_15","HH4V_14","HH4V_13","HH4V_12","HH4V_11","P1HH_15","P1HH_14","P1HH_13","P1HH_12","P1HH_11","P1HHNV_15","P1HHNV_14","P1HHNV_13","P1HHNV_12","P1HHNV_11","P1HH1V_15","P1HH1V_14","P1HH1V_13","P1HH1V_12","P1HH1V_11","P1HH2V_15","P1HH2V_14","P1HH2V_13","P1HH2V_12","P1HH2V_11","P1HH3V_15","P1HH3V_14","P1HH3V_13","P1HH3V_12","P1HH3V_11","P1HH4V_15","P1HH4V_14","P1HH4V_13","P1HH4V_12","P1HH4V_11","P2HH_15","P2HH_14","P2HH_13","P2HH_12","P2HH_11","P2HHNV_15","P2HHNV_14","P2HHNV_13","P2HHNV_12","P2HHNV_11","P2HH1V_15","P2HH1V_14","P2HH1V_13","P2HH1V_12","P2HH1V_11","P2HH2V_15","P2HH2V_14","P2HH2V_13","P2HH2V_12","P2HH2V_11","P2HH3V_15","P2HH3V_14","P2HH3V_13","P2HH3V_12","P2HH3V_11","P2HH4V_15","P2HH4V_14","P2HH4V_13","P2HH4V_12","P2HH4V_11","P3HH_15","P3HH_14","P3HH_13","P3HH_12","P3HH_11","P3HHNV_15","P3HHNV_14","P3HHNV_13","P3HHNV_12","P3HHNV_11","P3HH1V_15","P3HH1V_14","P3HH1V_13","P3HH1V_12","P3HH1V_11","P3HH2V_15","P3HH2V_14","P3HH2V_13","P3HH2V_12","P3HH2V_11","P3HH3V_15","P3HH3V_14","P3HH3V_13","P3HH3V_12","P3HH3V_11","P3HH4V_15","P3HH4V_14","P3HH4V_13","P3HH4V_12","P3HH4V_11","P4HH_15","P4HH_14","P4HH_13","P4HH_12","P4HH_11","P4HHNV_15","P4HHNV_14","P4HHNV_13","P4HHNV_12","P4HHNV_11","P4HH1V_15","P4HH1V_14","P4HH1V_13","P4HH1V_12","P4HH1V_11","P4HH2V_15","P4HH2V_14","P4HH2V_13","P4HH2V_12","P4HH2V_11","P4HH3V_15","P4HH3V_14","P4HH3V_13","P4HH3V_12","P4HH3V_11","P4HH4V_15","P4HH4V_14","P4HH4V_13","P4HH4V_12","P4HH4V_11"];
+var expressed = attrArray[0];
 
 window.onload = setMap();
 function setMap(){
@@ -30,36 +34,22 @@ function setMap(){
       // Loading attributes from the csv file
       .defer(d3.csv, "data/DC_Metropolitan.csv")
       // Loading background spatial data
-      .defer(d3.json, "data/ContinentalUS_CT.topojson")
+      .defer(d3.json, "data/DC_States.topojson")
       // Loading spatial data
       .defer(d3.json, "data/DC_Metropolitan.topojson")
       .await(callback);
 
   function callback(error, csvData, unitedstates, dcmetropolitan){
     // Creating a generator for the graticule
-    var graticule = d3.geoGraticule()
-      .step([5,5]);
 
-    // Creating a background for the graticules
-    var gratBackground = map.append("path")
-      .datum(graticule.outline())
-      .attr("class", "gratBackground")
-      .attr("d", path);
-
-    // Creating the graticules for the map
-    var gratLines = map.selectAll(".gratLines")
-      .data(graticule.lines())
-      .enter()
-      .append("path")
-      .attr("class", "gratLines")
-      .attr("d", path);
+    setGraticule(map, path);
 
     // Examining the results to ensure that the callback parameters are loaded properly
-    console.log(error);
+    // console.log(error);
     console.log(csvData);
 
     // Translating the United States and Washington DC TopoJSONs to GeoJSONs
-    var us = topojson.feature(unitedstates, unitedstates.objects.ContinentalUS_CT),
+    var us = topojson.feature(unitedstates, unitedstates.objects.DC_States),
         dcArea = topojson.feature(dcmetropolitan, dcmetropolitan.objects.DC_Metropolitan).features;
 
     // Adding the United States Census Tracts to the map
@@ -68,17 +58,92 @@ function setMap(){
         .attr("class", "states")
         .attr("d", path);
 
-    // Adding the census tracts for the Washington DC Metropolitan Area
-    var censustracts = map.selectAll(".CensusTract")
-        .data(dcArea)
-        .enter()
-        .append("path")
-        .attr("class", function(d){
-          return "CensusTract " + d.properties.NAME;
-        })
-        .attr("d", path);
+    dcArea = joinData(dcArea, csvData);
 
+    var colorScale = makeColorScale(csvData);
+    setEnumerationUnits(dcArea, map, path, colorScale);
     console.log(us);
     console.log(dcArea);
   };
 };
+
+function setGraticule(map, path){
+  var graticule = d3.geoGraticule()
+    .step([5,5]);
+
+  // Creating a background for the graticules
+  var gratBackground = map.append("path")
+    .datum(graticule.outline())
+    .attr("class", "gratBackground")
+    .attr("d", path);
+
+  // Creating the graticules for the map
+  var gratLines = map.selectAll(".gratLines")
+    .data(graticule.lines())
+    .enter()
+    .append("path")
+    .attr("class", "gratLines")
+    .attr("d", path);
+
+};
+
+function joinData(dcArea, csvData){
+  for (var i=0; i<csvData.length; i++){
+    var csvCensusTract = csvData[i];
+    var csvKey = csvCensusTract.NAME;
+
+    for (var a = 0; a < dcArea.length; a++){
+
+      var geojsonProps = dcArea[a].properties;
+      var geojsonKey = geojsonProps.NAME;
+
+      if (geojsonKey == csvKey){
+        attrArray.forEach(function(attr){
+          var val = parseFloat(csvCensusTract[attr]);
+          geojsonProps[attr] = val;
+        });
+      };
+    };
+  };
+  return dcArea;
+};
+
+function makeColorScale(data){
+  var colorClasses = [
+    "#D4B9DA",
+    "#C994C7",
+    "#DF65B0",
+    "#DD1C77",
+    "#980043"
+  ];
+
+  var colorScale = d3.scaleQuantile()
+      .range(colorClasses);
+
+  //build two-value array of minimum and maximum expressed attribute values
+  var minmax = [
+      d3.min(data, function(d) { return parseFloat(d[expressed]); }),
+      d3.max(data, function(d) { return parseFloat(d[expressed]); })
+  ];
+  //assign two-value array as scale domain
+  colorScale.domain(minmax);
+
+  return colorScale;
+};
+
+function setEnumerationUnits(dcArea, map, path, colorScale){
+
+  // Adding the census tracts for the Washington DC Metropolitan Area
+  var censustracts = map.selectAll(".CensusTract")
+      .data(dcArea)
+      .enter()
+      .append("path")
+      .attr("class", function(d){
+        return "CensusTract " + d.properties.NAME;
+      })
+      .attr("d", path)
+      .style("CensusTract", function(d){
+        return colorScale(d.properties[expressed]);
+      });
+};
+})();
